27eec22714801ed107c18e61b8fdf9afed2ec168
nguyenuy1409 <nguyenuyy1409@gmail.com>
2026-07-16 22:33:48 +1000

    test: add FIFO priority test case

--- tests/OrderBookTest.cpp
+++ tests/OrderBookTest.cpp
@@ -37,11 +37,22 @@ TEST(OrderBookTest, PartialFill) {
 
 TEST(OrderBookTest, CancelOrder) {
     Order bid("001", "Nuke", 667, "limit", "bid", 69, "day");
-    Order ask("003", "Nuke", 67, "limit", "ask", 69, "day");
+    Order ask("002", "Nuke", 67, "limit", "ask", 69, "day");
     OrderBook book;
     book.insert(bid);
     book.cancel(bid.getOrderId());
     book.insert(ask);
     EXPECT_EQ(book.getTopOfBook().first, -1);
     EXPECT_EQ(book.getTopOfBook().second, 0);
+}
+
+TEST(OrderBookTest, FIFO_Test) {
+    Order bid("001", "Nuke", 70, "limit", "bid", 167, "day");
+    Order ask1("002", "Nuke", 670, "limit", "ask", 69, "day");
+    Order ask2("003", "Nuke", 690, "limit", "ask", 69, "day");
+    OrderBook book;
+    book.insert(bid);
+    book.insert(ask1);
+    book.insert(ask2);
+    EXPECT_EQ(book.getBestAskQuantity(), 600);
 }
